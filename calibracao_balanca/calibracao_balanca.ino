020293b10022030baa93f178ac54e71608927ebf
Davi Schmitz <davi.schmitz@hotmail.com>
2023-11-20 23:39:33 -0300

    Remove unnecessary comments

--- calibracao_balanca/calibracao_balanca.ino
+++ calibracao_balanca/calibracao_balanca.ino
@@ -3,22 +3,22 @@
 #define DT A1
 #define SCK A0
 
-HX711 escala; // Relaciona a variável escala
+HX711 escala; 
 
 void setup() {
  escala.begin (DT, SCK);
  Serial.begin(9600);
  Serial.print("Leitura da Tara: ");
- Serial.println(escala.read()); // Aguada o termino de verificação do peso
+ Serial.println(escala.read());
  Serial.println("Aguarde!");
  Serial.println("Iniciando ...");
- escala.set_scale(); // Utiliza uma escala padrão de verificação
+ escala.set_scale(); 
 
- escala.tare(20); // Fixa o peso como tara
+ escala.tare(20);
  Serial.println("Insira o item para Pesar");
 }
 
 void loop() {
  Serial.print("Valor da Leitura: ");
- Serial.println(escala.get_value(10),0); // Retorna peso descontada a tara
+ Serial.println(escala.get_value(10), 0); 
 }
